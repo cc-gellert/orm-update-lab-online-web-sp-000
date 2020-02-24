324d0c893d3e66bac9c476f1553ef6088279ddb4
Chloe Gell <gellert.chloe@gmail.com>
2020-02-24 18:31:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,6 +38,6 @@ class Student
     VALUES (?, ?) 
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute()[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end 
 end
