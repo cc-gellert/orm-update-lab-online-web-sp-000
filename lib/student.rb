ac08c6067fa32c344ca16d86948825eeb1e9416f
Chloe Gell <gellert.chloe@gmail.com>
2020-02-24 18:32:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,4 +40,9 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end 
+  
+  def create(name, grade)
+    sql = <<-SQL
+    INSERT INTO students 
+  end 
 end
